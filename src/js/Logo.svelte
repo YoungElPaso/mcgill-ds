97400054ba9b2583278d49ed6d2bfeb1c255e3af
Jesse Sutherland <jesse.sutherland@mcgill.ca>
2019-11-27 12:26:23 -0500

    Added Logo component - demonstrates a WIP component first authoring approach -all CSS within SFC (single file component).

---
+++ src/js/Logo.svelte
@@ -0,0 +1,35 @@
+<script>
+  // A size variable for the logo. Available in small, medium, large.
+  // Large is the default value.
+  export let size = "large";
+
+  // Import logo image in JS. Makes sense actually, since HTML in this component should be treated as is -ie. relative path to where the component is rendered, not where it lives in source!
+  import logo from '../../assets/mcgill-logo-red-reverse-XL-header.svg';
+</script>
+
+<!-- This uses a direct reference to a variable and Parcel does resolve the path right path, good! -->
+<div class="logo {size}">
+  <img class={size} src="{logo}" alt="McGill Logo" />
+</div>
+
+
+<style>
+  .logo {
+    float: left;
+    background: red;
+    padding: 1em;
+    margin-right: 1em;
+  }
+
+  .small {
+    width: 50px;
+  }
+
+  .medium {
+    width: 100px;
+  }
+
+  .large {
+    width: 200px;
+  }
+</style>
