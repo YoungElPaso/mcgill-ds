041bc64625ba6073110c43df704a0e23bd61fe0d
Jesse Sutherland, Mr <jesse.sutherland@mcgill.ca>
2021-02-18 08:39:18 -0500

    Added tailwind, jest, babel, postcss config files, see WMS-23681.

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+// Import the TW config.
+const twConfig = require("./tailwind.config.js");
+
+module.exports = {
+  // If need to specifically set a 'from' and 'to' option for postcss:
+  // from: "src/css/mds.css",
+  // to: "dist/css/mds.css",
+  plugins: {
+    // Use the tailwindcss plugin with twConfig.
+    tailwindcss: twConfig || null,
+
+    // Use autoprefixer with default config.
+    autoprefixer: {} || null,
+
+    // Use cssnano (miniferi) with cssnano-preset-default package.
+    // TODO: this should only happen on NODE_ENV = PRODUCTION.
+    cssnano: {
+      preset: [require("cssnano-preset-default")]
+    }
+  }
+};
